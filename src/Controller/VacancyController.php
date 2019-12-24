f5a963dad424896ef38c0b7cb7c5f2e585fa83ab
Nikolio1 <nikolio.first@gmail.com>
2019-12-24 15:53:51 +0200

    edited crud vacancy

--- src/Controller/VacancyController.php
+++ src/Controller/VacancyController.php
@@ -5,24 +5,25 @@ namespace App\Controller;
 use App\Entity\ResponseVacancy;
 use App\Entity\Vacancy;
 use App\Form\Type\ResponseVacancyType;
+use App\Form\Type\VacancyType;
 use App\Handlers\VacancyHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class VacancyController extends AbstractController
 {
-
-    public $handler;
+    public $vacancyHandler;
 
     /**
      * VacancyController constructor.
-     * @param VacancyHandler $handler
+     * @param VacancyHandler $vacancyHandler
      */
-    public function __construct(VacancyHandler $handler)
+    public function __construct(VacancyHandler $vacancyHandler)
     {
-        $this->handler = $handler;
+        $this->vacancyHandler = $vacancyHandler;
     }
 
     /**
@@ -33,7 +34,7 @@ class VacancyController extends AbstractController
      */
     public function vacancies(Request $request)
     {
-        $vacancies = $this->handler
+        $vacancies = $this->vacancyHandler
             ->getRepository(Vacancy::class)
             ->findAll();
 
@@ -43,7 +44,7 @@ class VacancyController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->handler->saveObject($response);
+            $this->vacancyHandler->saveObject($response);
             $this->addFlash('success', 'new item created successfully!!!');
 
             return $this->redirectToRoute('vacancies');
@@ -53,6 +54,82 @@ class VacancyController extends AbstractController
         return $this->render('vacancy/index.html.twig', [
             'vacancies' => $vacancies,
             'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route("/vacancy/{id}", name="show_vacancy")
+     * @param Vacancy $vacancy
+     * @return Response
+     */
+    public function show(Vacancy $vacancy)
+    {
+        return $this->render('vacancy/vacancy.html.twig', [
+            'vacancy' => $vacancy
+        ]);
+    }
+
+    /**
+     * @Route("/delete-vacancy/{id}", name="delete_vacancy")
+     *
+     * @param Vacancy $post
+     *
+     * @return RedirectResponse
+     */
+    public function delete(Vacancy $post)
+    {
+        if (!$post) {
+            return $this->redirectToRoute('vacancies');
+        }
+        $this->vacancyHandler->removeObject($post);
+        return $this->redirectToRoute('vacancies');
+    }
+
+    /**
+     * @Route("/create-vacancy", name="create_vacancy")
+     *
+     * @param Request $request
+     *
+     * @return RedirectResponse|Response
+     */
+    public function new(Request $request)
+    {
+        $vacancy = new Vacancy();
+        $form = $this->createForm(VacancyType::class, $vacancy);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->vacancyHandler->saveObject($vacancy);
+            $this->addFlash('success', 'new item created success!!!');
+            return $this->redirectToRoute('vacancies');
+        }
+        return $this->render('vacancy/createVacancy.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/edit-vacancy/{id}", name="edit_vacancy")
+     *
+     * @param vacancy $vacancy
+     * @param Request $request
+     *
+     * @return RedirectResponse|Response
+     */
+    public function edit(vacancy $vacancy, Request $request)
+    {
+        $vacancy = $this->vacancyHandler
+            ->getRepository(vacancy::class)
+            ->find($vacancy);
+        $form = $this->createForm(VacancyType::class, $vacancy);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->vacancyHandler->saveObject($vacancy);
+            $this->addFlash('success', 'Success)))!');
+            return $this->redirectToRoute('vacancies');
+        }
+        return $this->render('vacancy/editVacancy.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }

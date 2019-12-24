4a37048fccd2938017618e43d7c3d6db041a4a72
Nikolio1 <nikolio.first@gmail.com>
2019-12-24 12:37:48 +0200

    edited crud news

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\News;
+use App\Form\Type\NewsType;
 use App\Handlers\NewsHandler;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -54,6 +56,69 @@ class NewsController extends AbstractController
     {
         return $this->render('news/news.html.twig', [
             'news' => $news
+        ]);
+    }
+
+    /**
+     * @Route("/delete-news/{id}", name="delete_news")
+     *
+     * @param News $post
+     *
+     * @return RedirectResponse
+     */
+    public function delete(News $post)
+    {
+        if (!$post) {
+            return $this->redirectToRoute('categories');
+        }
+        $this->newsHandler->removeObject($post);
+        return $this->redirectToRoute('all_news');
+    }
+
+    /**
+     * @Route("/create-news", name="create_news")
+     *
+     * @param Request $request
+     *
+     * @return RedirectResponse|Response
+     */
+    public function new(Request $request)
+    {
+        $news = new News();
+        $form = $this->createForm(NewsType::class, $news);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->newsHandler->saveObject($news);
+            $this->addFlash('success', 'new item created success!!!');
+            return $this->redirectToRoute('all_news');
+        }
+        return $this->render('news/newNews.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/edit-news/{id}", name="edit_news")
+     *
+     * @param News $news
+     * @param Request $request
+     *
+     * @return RedirectResponse|Response
+     */
+    public function edit(News $news, Request $request)
+    {
+        $news = $this->newsHandler
+            ->getRepository(News::class)
+            ->find($news);
+        $form = $this->createForm(NewsType::class, $news);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->newsHandler->saveObject($news);
+            $this->addFlash('success', 'Success)))!');
+            return $this->redirectToRoute('all_news');
+        }
+        return $this->render('news/editNews.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }

46071724de1bb8157daba47a3f48669a1130b790
Nikolio1 <nikolio.first@gmail.com>
2019-12-24 11:01:14 +0200

    edited crud category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\Type\CategoryType;
 use App\Handlers\CategoryHandler;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -59,4 +61,69 @@ class CategoryController extends AbstractController
             'category' => $category
         ]);
     }
+
+    /**
+     * @Route("/delete-category/{id}", name="delete_category")
+     *
+     * @param Category $post
+     *
+     * @return RedirectResponse
+     */
+    public function delete(Category $post)
+    {
+        if (!$post) {
+            return $this->redirectToRoute('categories');
+        }
+        $this->categoryHandler->removeObject($post);
+        return $this->redirectToRoute('categories');
+    }
+
+    /**
+     * @Route("/new-category", name="new_category")
+     *
+     * @param Request $request
+     *
+     * @return RedirectResponse|Response
+     */
+    public function new(Request $request)
+    {
+        $category= new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->categoryHandler->saveObject($category);
+            $this->addFlash('success', 'new item created success!!!');
+            return $this->redirectToRoute('categories');
+        }
+        return $this->render('category/newCategory.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/edit-category/{id}", name="edit_category")
+     *
+     * @param Category $category
+     * @param Request $request
+     *
+     * @return RedirectResponse|Response
+     */
+    public function edit(Category $category, Request $request)
+    {
+        $category = $this->categoryHandler
+            ->getRepository(Category::class)
+            ->find($category);
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->categoryHandler->saveObject($category);
+            $this->addFlash('success', 'Success)))!');
+            return $this->redirectToRoute('categories');
+        }
+        return $this->render('category/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
 }
--- src/Entity/SubCategory.php
+++ src/Entity/SubCategory.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -33,7 +35,22 @@ class SubCategory
      *     nullable=false
      *)
      */
+
     private $category;
+
+    /**
+     * @ORM\OneToMany(
+     *     targetEntity="Product",
+     *     mappedBy="subCategory",
+     *     cascade={"persist","remove"}
+     * )
+     */
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -60,6 +77,37 @@ class SubCategory
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Product[]
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): self
+    {
+        if (!$this->products->contains($product)) {
+            $this->products[] = $product;
+            $product->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): self
+    {
+        if ($this->products->contains($product)) {
+            $this->products->removeElement($product);
+            // set the owning side to null (unless already changed)
+            if ($product->getCategory() === $this) {
+                $product->setCategory(null);
+            }
+        }
 
         return $this;
     }

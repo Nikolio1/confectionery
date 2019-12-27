880be70a97d314786c7fb1f57336d01fff000493
Nikolio1 <nikolio.first@gmail.com>
2019-12-27 14:52:22 +0200

    sending message in contacts

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Entity\Review;
+use App\Form\Type\ContactType;
+use App\Handlers\BaseHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -14,6 +20,48 @@ class ContactController extends AbstractController
     {
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
+        ]);
+    }
+
+    /**
+     * @var BaseHandler
+     */
+    public $handler;
+
+    /**
+     * ReviewController constructor.
+     * @param BaseHandler $handler
+     */
+    public function __construct(BaseHandler $handler)
+    {
+        $this->handler = $handler;
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function reviews(Request $request)
+    {
+
+        $response = new Contact();
+        $form = $this->createForm(ContactType::class, $response);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->handler->saveObject($response);
+            $this->addFlash('successContact', 'Your message has been sent successfully');
+
+            return $this->redirectToRoute('contact');
+        }
+
+        return $this->render('contact/contact.html.twig', [
+            'title' => 'Contacts',
+            'form' => $form->createView()
         ]);
     }
 }

1dba685559af7ec2c0f22b43a49d10ccd1e3b5dd
Nikolio1 <nikolio.first@gmail.com>
2020-01-06 11:27:04 +0200

    Authenticating Users

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * Class UserFixtures
+ *
+ * @package App\DataFixtures
+ */
+class UserFixtures extends BaseFixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    protected function loadData(ObjectManager $manager)
+    {
+        $this->createMany(User::class, 1,function (User $user, $count) {
+
+            $user->setEmail('user1@mail.ru')
+                  ->setFirstName($this->faker->firstName)
+                  ->setPassword($this->passwordEncoder->encodePassword(
+                      $user,
+                      '1234'
+                  ));
+        });
+
+        $manager->flush();
+
+    }
+}
